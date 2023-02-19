cb49564402295748a69cd5341251e182252f9b1c
Yy2z <1615978027@qq.com>
2023-02-19 19:19:14 +0800

    更新

--- api/index.py
+++ api/index.py
@@ -7,18 +7,14 @@ def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
 
 def getdata(name):
-    gitpage = requests.get("https://github.com/" + name)
+    gitpage = requests.get(f"https://github.com/{name}")
     data = gitpage.text
     datadatereg = re.compile(r'data-date="(.*?)" data-level')
     datacountreg = re.compile(r'data-count="(.*?)" data-date')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
-    datacount = list(map(int, datacount))
-    contributions = sum(datacount)
-    datalist = []
-    for index, item in enumerate(datadate):
-        itemlist = {"date": item, "count": datacount[index]}
-        datalist.append(itemlist)
+    contributions = sum(map(int, datacount))
+    datalist = [{"date": date, "count": int(count)} for date, count in zip(datadate, datacount)]
     datalistsplit = list_split(datalist, 7)
     return {"total": contributions, "contributions": datalistsplit}
 

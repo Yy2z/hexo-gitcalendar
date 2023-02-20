a1cecb7cad1a0995c098990a4183b84e27f6ebb9
Yy2z <1615978027@qq.com>
2023-02-20 09:54:15 +0800

    更新

--- api/index.py
+++ api/index.py
@@ -1,14 +1,19 @@
-import requests
 import re
-from http.server import BaseHTTPRequestHandler
 import json
+from http.server import BaseHTTPRequestHandler
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
 
 def getdata(name):
-    gitpage = requests.get(f"https://github.com/{name}")
-    data = gitpage.text
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(options=options)
+    driver.get(f"https://github.com/{name}")
+    data = driver.page_source
     datadatereg = re.compile(r'data-date="(.*?)" data-level')
     datacountreg = re.compile(r'data-count="(.*?)" data-date')
     datadate = datadatereg.findall(data)
@@ -16,6 +21,7 @@ def getdata(name):
     contributions = sum(map(int, datacount))
     datalist = [{"date": date, "count": int(count)} for date, count in zip(datadate, datacount)]
     datalistsplit = list_split(datalist, 7)
+    driver.quit()
     return {"total": contributions, "contributions": datalistsplit}
 
 class handler(BaseHTTPRequestHandler):
@@ -23,10 +29,7 @@ class handler(BaseHTTPRequestHandler):
         path = self.path
         user = path.split('?')[1]
         data = getdata(user)
-        if "error" in data:
-            self.send_response(404)
-        else:
-            self.send_response(200)
+        self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
